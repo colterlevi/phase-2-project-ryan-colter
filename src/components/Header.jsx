3bbadd7f71f051186a0d7f4f014a9dfed0a2e9ec
colterlevi <cllongshore@gmail.com>
2022-12-05 13:49:51 -0500

    burger menu take 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,19 @@
+// import HamburgerMenu from './components/HamburgerMenu'
+import { slide as Menu } from 'react-burger-menu'
 
+const HamburgerMenu = () => {
+    return (
+        <Menu>
+            <p>login</p><br />
+            <p>leaderboard</p>
+        </Menu>
+    );
+}
 
 const Header = () => {
     return(
         <div className="header">
+            <HamburgerMenu />
             <h1>Trivia Masters</h1>
             <nav>login</nav>
             <nav>Search</nav>
