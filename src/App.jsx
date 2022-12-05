25e49326dfebe6671b3586f173c77ccc57cbbc92
colterlevi <cllongshore@gmail.com>
2022-12-05 11:38:23 -0500

    use effect be effectin the use

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 const App = () => {
+  const [questions, setQuestions] = useState([])
+  const [category, setCategory] = useState("history")
+  console.log(category)
+  
+  useEffect(() => {
+    const request = async () => {
+      let req = await fetch(`https://the-trivia-api.com/api/questions?categories=${category}&limit=5`)
+      let res = await req.json()
+      setQuestions(res)
+    }
+    request()
+  },[category])
+
+  const Questions = () => {
+    return(
+      <div>
+        {
+        questions.map((q) => {
+          return <p >{q.question}</p>
+        })
+        }
+
+      </div>
+    )
+  }
 
   return (
     <div className="App">
-      
+      <Questions />
+      <button onClick={() => { setCategory('music') }}>Music</button>
+      {console.log(category)}
     </div>
   )
 }
---
+++ src/components/TriviaCards.jsx
@@ -0,0 +1,9 @@
+import { useEffect, useState } from 'react'
+
+const TriviaCards = () => {
+    useEffect(() => {
+        const request = async () => {
+            let req = await fetch(``)
+        }
+    })
+}
