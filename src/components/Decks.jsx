910532d367f46f3992abaeb59854ebd38757bbdb
colterlevi <cllongshore@gmail.com>
2022-12-06 15:42:18 -0500

    bad styling

--- src/components/Decks.jsx
+++ src/components/Decks.jsx
@@ -1,20 +1,24 @@
 import { Titles } from '../../Titles'
 import { useState } from 'react'
+import { Link } from 'react-router-dom'
+
 const Decks = ({ setCategory, category }) => {
     const [deck, setDeck] = useState(Titles)
     console.log(category)
     console.log(Titles)
     return (
-        <div>
+        <div className="decks">
             {
                 deck.map((deck) => {
                     return (
-                        <div onClick={() => { setCategory(deck.Category) }}>
-                            <h1>{deck.Title}</h1>
+                        <Link className="gamecards" to="/game">
+                        <div className="card" onClick={() => { setCategory(deck.Category) }}>
+                            {/* <h1>{deck.Title}</h1> */}
                             <h2>{deck.Category}</h2>
                             <h3>{deck.Bio}</h3>
                             <img src={deck.Images} />
                         </div>
+                        </Link>
                     )
                 })
             }
