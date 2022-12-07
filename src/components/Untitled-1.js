44096240461357cb1edd0a8fcfba8ea5bfade1d4
Ryan Andujar <randujar711@gmail.com>
2022-12-07 13:22:03 -0500

    new stuff

---
+++ src/components/Untitled-1.js
@@ -0,0 +1,10 @@
+
+questions = [
+    {
+        question: 'What is higher than 4',
+        answers: [
+            {key: 'A', choice: '4', correct: false},
+            {key: 'B', choice: '5', correct: true},
+        ]
+    }
+]
