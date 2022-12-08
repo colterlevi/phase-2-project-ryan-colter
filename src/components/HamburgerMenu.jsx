e4a20e1b89266417e9d51065c54703b09728ed6a
colterlevi <cllongshore@gmail.com>
2022-12-08 15:33:44 -0500

    crown logo in menu

--- src/components/HamburgerMenu.jsx
+++ src/components/HamburgerMenu.jsx
@@ -4,6 +4,9 @@ import { slide as Menu } from 'react-burger-menu'
 const HamburgerMenu = () => {
     return (
         <Menu>
+            <div id='img-cont'>
+                <img src="src/assets/24-Crowns.svg" alt="Trivia Masters Crown" />
+            </div>
             <Link to="/">
                 <h3>Home</h3>
             </Link>
