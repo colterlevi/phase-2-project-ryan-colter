06fad38a357da7fe8d22d62121ff1ddcc1ddd07e
colterlevi <cllongshore@gmail.com>
2022-12-08 11:10:21 -0500

    form step

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -2,17 +2,26 @@ import { useState } from 'react'
 const Questions = ({ questions, setQuestions }) => {
     const [formPage, setFormPage] = useState('first')
     const [newArr, setNewArr] = useState([])
+    const [formStep, setFormStep] = useState("quiz")
+    const [formData, setFormData] = useState({
+        score: null
+    })
 
 
     const handleSubmit = async (e) => {
         e.preventDefault()
         // console.log("submitted")
-        // let req = await fetch('url', { method: 'PATCH' })
+        // let req = await fetch('http://localhost:3000/users', { method: 'PATCH' })
+    }
+
+    function handleChange(e) {
+        setFormData({ ...formData, [e.target.name]: e.target.value })
     }
 
     return (
         <div className='quiz'>
             <form onSubmit={(e) => { handleSubmit(e) }}>
+        { formStep === "quiz" && <div>
                 {
                     questions.map((obj) => {
                         let AllChoices = [...obj.incorrectAnswers, obj.correctAnswer]
@@ -34,8 +43,14 @@ const Questions = ({ questions, setQuestions }) => {
                     })
                 }
                 <br />
-                <hr />
-                <input className="quiz-btn" type="submit" />
+                <button className="quiz-btn" onClick={() => { setFormStep("score") }}>See Your Score</button>
+                </div>}
+                {formStep === "score" && <div>
+                    <h2>Score!</h2>
+                    <input className="quiz-input" type='text' placeholder='enter your score' value={formData.score} name='score' onChange={e => handleChange(e)} ></input>
+                    <input className="quiz-btn" type="submit" />
+                </div>
+                }
             </form>
         </div>
     )
